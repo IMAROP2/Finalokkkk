4f1f4ef32d03f6cbbc7ebf553b82ef928562fca4
IMAROP2 <rishabh.singh.riti.singh.123@gmail.com>
2024-09-08 12:35:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let selectedGame = null;
 
     sourceCode.addEventListener('click', () => {
-        window.open('https://github.com/ShafiqSadat/HamsterKeyGenWeb', '_blank');
+        window.open('https://t.me/HamesterKombatdailyUpdates', '_blank');
     });
 
     gameOptions.forEach(option => {
